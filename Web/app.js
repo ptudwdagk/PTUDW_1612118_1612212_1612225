87a1b5ac3473c925298db99fa0e498095a5000e4
phihung312 <44272115+phihung312@users.noreply.github.com>
2019-06-10 23:17:14 +0700

    BE

---
+++ Web/app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+var morgan = require('morgan');
+
+var app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.engine('hbs', exphbs({
+  defaultLayout: 'main.hbs',
+  layoutsDir: 'views/_layouts'
+}));
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname+'/public'));
+// app.use(require('./middlewares/locals.mdw'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+app.get('/KinhDoanh', (req, res) => {
+  res.render('KinhDoanh');
+})
+
+
+
+
+// app.use('/categories', require('./routes/category.route'));
+// app.use('/admin/categories', require('./routes/admin/category.route'));
+
+app.listen(4000, () => {
+  console.log('Web Server is running at http://localhost:4000');
+})
