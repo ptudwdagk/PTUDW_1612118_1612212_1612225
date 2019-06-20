9a8ce68fed0c6082eef42fd8f50e2cf80583baae
phihung312 <44272115+phihung312@users.noreply.github.com>
2019-06-20 17:16:44 +0700

    Hieu chinh bai viet writer

--- Web/models/Baivietdaxuatban.model.js
+++ Web/models/Baivietdaxuatban.model.js
@@ -1,8 +1,17 @@
 var db =require('../utils/db')
 module.exports= {
-    daxuatban: () => {
-        return db.load(`select * from baibao where TacGia=4 and TrangThai=1 and Xoa!=1`);
+    daxuatban:id  => {
+        return db.load(`select * from baibao where TacGia= ${id} and TrangThai=1 and Xoa!=1`);
     },
+    dangchoxuatbantheowriter: id =>{
+       return db.load(`select * from baibao where TacGia= ${id} and TrangThai=2 and Xoa!=1 `);
+    },
+    chuaduocduyettheowriter: id =>{
+        return db.load(`select * from baibao where TacGia= ${id} and TrangThai=3 and Xoa!=1 `);
+     },
+     bituchoitheowriter: id =>{
+        return db.load(`select * from baibao where TacGia= ${id} and TrangThai=4 and Xoa!=1 `);
+     },
     dangchoxuatban: id  =>{
         return db.load(`select bb.idBaiBao, bb.TenBaiBao,bb.NgayDang,tv.HoTen,cm.TenCM from baibao bb, chuyenmuc cm, thanhvien tv where bb.ChuyenMuc=cm.idChuyenMuc and bb.TacGia=tv.idThanhVien and bb.TrangThai=3 and bb.Xoa!=1 and bb.ChuyenMuc= ${id}` )
     },
--- Web/models/tagmodel.js
+++ Web/models/tagmodel.js
@@ -3,5 +3,12 @@ var db=require('../utils/db')
 module.exports={
     add: entity => {
         return db.add ('nhantag',entity);
-    }
+    },
+    single: id => {
+    return db.load(`select * from nhantag nt join tag_baibao tbb on tbb.idTag = nt.idTag  where idBaiBao = ${id}   `);
+    
+    },
+    update: entity => {
+        return db.update ('nhantag','idTag',entity);
+    },
 }
